5fd38ffa38b905f50480a86ad3c9207f59cba852
Yatharth Gouri <40733546+yatharthgouri@users.noreply.github.com>
2020-06-02 02:49:56 +0530

    Create Plot1.R

---
+++ Project1/Plot1.R
@@ -0,0 +1,8 @@
+# Set the Working Directory
+setwd("~//ExData_Plotting1")
+
+source("Load_data.R")
+png(filename = "Plot1.png", width = 480, height = 480, units = "px")
+hist(Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)", breaks = 12, ylim = c(0, 1200))
+dev.off()
+rm(data)
