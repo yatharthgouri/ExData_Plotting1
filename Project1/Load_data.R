d341d1f7909038e8fd2d14003a29a74436c694fe
Yatharth Gouri <40733546+yatharthgouri@users.noreply.github.com>
2020-06-02 02:51:03 +0530

    Update Load_data.R

--- Project1/Load_data.R
+++ Project1/Load_data.R
@@ -1,3 +1,6 @@
+# Set the Working Directory
+setwd("~/GitHub/ExData_Plotting1")
+
 # Read the Data to a data frame
 data <- read.csv(file = "./data/PowerConsumption.csv", header = TRUE)
 attach(data)
